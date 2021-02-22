dbe551c4388db9ca9394a7b92ecf4273faf56e12
ARYA RAJPUT <rajputarya10@gmail.com>
2021-02-22 23:26:45 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './css/home.css'
+import TinderCard from 'react-tinder-card'
+
+
+function Home() {
+    return (
+        <div className='Home'>
+     <div className='header'>
+        
+         
+         </div> 
+         <div className='main'>
+             <TinderCard ></TinderCard>
+             </div>   
+             <div className='footer'>
+            
+
+                 </div>    
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Icons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/icon.css'
+
+function Options({icon,name,shadow}) {
+    return (
+        <div className='icon' >
+            {icon}
+        </div>
+    )
+}
+
+export default Options
