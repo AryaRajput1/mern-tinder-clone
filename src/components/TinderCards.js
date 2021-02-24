91cd2aded57654e93ec80fad65b5b369015ab99b
ARYA RAJPUT <rajputarya10@gmail.com>
2021-02-24 16:40:05 +0530

    final commit

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React ,{useState,useEffect}from 'react'
 import TinderCard from 'react-tinder-card'
 import './css/tindercard.css'
-
+import axios from 'axios'
 function TinderCards() {
+    const [data, setdata] = useState(null)
+   
+   useEffect(() => {
+//    async function fetchdata(){ 
+//       const xios= axios.create({
+//            baseURL:'http://localhost:8001'
+//        });
+       axios.get('http://localhost:8001/tinder/card').then(data=>{
+           setdata(data.data);
+       })
+       
+       
+   }, [])
+   
     const swipped=(dir)=>{
         console.log(dir);
     }
     const cardLeft=(name)=>{
 console.log(name+'left');
     }
-    const data=[{
-        name:'Salman',
-        url:'https://upload.wikimedia.org/wikipedia/commons/8/86/Salman_Khan_at_Renault_Star_Guild_Awards.jpg'
-    },{
-        name:'Akshay',
-        url:'https://timesofindia.indiatimes.com/thumb/msid-72168734,width-800,height-600,resizemode-4/72168734.jpg?imglength=277599'
-    },{
-        name:'Tiger',
-        url:'https://filmfare.wwmindia.com/content/2020/aug/tigershroff11598695436.jpg'
-    },{
-        name:'Hritik',
-        url:'https://static.toiimg.com/photo/msid-71756742/71756742.jpg?134917'
-    }]
+    
     return (
         <div className='tindercard'>
-{
+{ !data?<h1>loading</h1>:
     data.map(a=>{
        return <TinderCard key={a.name} onSwipe={swipped} onCardLeftScreen={()=>cardLeft(a.name)} preventSwipe={['up','down']}><div className='card' style={{backgroundImage:`url(${a.url})`}}><h4>{a.name}</h4></div></TinderCard>
     })
